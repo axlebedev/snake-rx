d85aebcf23bda2f175f2449cd4bbfee922f71969
Alex Lebedev <alexey.broadcast@gmail.com>
2024-07-08 23:05:18 +0300

    process touchmove events

--- src/store/main.store.js
+++ src/store/main.store.js
@@ -1,4 +1,9 @@
-import { fromEvent, BehaviorSubject } from 'rxjs'
+import {
+  fromEvent,
+  BehaviorSubject,
+  switchMap,
+  map,
+} from 'rxjs'
 
 import { directions } from '@/consts'
 
@@ -9,28 +14,72 @@ import { message$ } from './message.store'
 
 import { doNextTurn, getNewApple } from './logic'
 
+const onNewDirection = (newDirection) => {
+  const currentDirection = direction$.value.current
+  if (newDirection === directions.bottom && currentDirection !== directions.top) {
+    direction$.next({ current: currentDirection, next: directions.bottom })
+  }
+  if (newDirection === directions.top && currentDirection !== directions.bottom) {
+    direction$.next({ current: currentDirection, next: directions.top })
+  }
+  if (newDirection === directions.left && currentDirection !== directions.right) {
+    direction$.next({ current: currentDirection, next: directions.left })
+  }
+  if (newDirection === directions.right && currentDirection !== directions.left) {
+    direction$.next({ current: currentDirection, next: directions.right })
+  }
+}
+
 export const startGame = () => {
   const keyDownsSubscription = fromEvent(document, 'keydown')
-    .subscribe((keydown) => {
-      const currentDirection = direction$.value.current
-      if (keydown.code === 'ArrowDown' && currentDirection !== directions.top) {
-        direction$.next({ current: currentDirection, next: directions.bottom })
-      }
-      if (keydown.code === 'ArrowUp' && currentDirection !== directions.bottom) {
-        direction$.next({ current: currentDirection, next: directions.top })
-      }
-      if (keydown.code === 'ArrowLeft' && currentDirection !== directions.right) {
-        direction$.next({ current: currentDirection, next: directions.left })
-      }
-      if (keydown.code === 'ArrowRight' && currentDirection !== directions.left) {
-        direction$.next({ current: currentDirection, next: directions.right })
-      }
-    })
+    .pipe(
+      map((keydown) => {
+        switch (keydown.code) {
+        case 'ArrowDown':
+          return directions.bottom
+        case 'ArrowUp':
+          return directions.up
+        case 'ArrowLeft':
+          return directions.left
+        case 'ArrowRight':
+        default:
+          return directions.right
+        }
+      }),
+    )
+    .subscribe(onNewDirection)
+
+  fromEvent(document, 'touchstart')
+    .pipe(
+      switchMap((startEvent) => {
+        startEvent.preventDefault()
+        startEvent.stopImmediatePropagation()
+
+        return fromEvent(document, 'touchend')
+          .pipe(
+            map((event) => {
+              const start = startEvent.touches[0]
+              const end = event.changedTouches[0]
+              return {
+                dLeft: end.clientX - start.clientX,
+                dTop: end.clientY - start.clientY,
+              }
+            }),
+            map(({ dLeft, dTop }) => {
+              if (Math.abs(dLeft) > Math.abs(dTop)) {
+                return dLeft < 0 ? directions.left : directions.right
+              }
+              return dTop < 0 ? directions.top : directions.bottom
+            }),
+          )
+      }),
+    )
+    .subscribe(onNewDirection)
 
   const mainInterval$ = new BehaviorSubject(1000)
 
   const intervalSubscription = mainInterval$
-    .subscribe((value) => {
+    .subscribe(() => {
       const snakeSegments = snakeSegments$.value
       const direction = direction$.value.next
       const apple = apple$.value
